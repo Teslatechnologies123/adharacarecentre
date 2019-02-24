f0d6209837cd3c9cddd394d3cb9cc5b0b71d1058
teslatechnologies123 <teslatechnologies123@outlook.com>
2019-02-24 14:35:43 +0530

    firstcommit

---
+++ sample.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Prometeo - Business, Financial and Consulting Site Template">
+	<meta name="author" content="Ansonika">
+	<title>Adhara Care Centre</title>
+
+	<!-- Favicons-->
+	<link rel="shortcut icon" href="img/final_logo.png" type="image/x-icon">
+	<link rel="apple-touch-icon" type="image/x-icon" href="img/final_logo.png">
+	<link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/final_logo.png">
+	<link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/final_logo.png">
+	<link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/final_logo.png">
+
+	<!-- BASE CSS -->
+	<link href="css/animate.min.css" rel="stylesheet">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/icon_fonts/css/all_icons_min.css" rel="stylesheet">
+
+	<!-- YOUR CUSTOM CSS -->
+	<link href="css/custom.css" rel="stylesheet">
+	
+	<!-- LayerSlider stylesheet -->
+	<link href="layerslider/css/layerslider.css" rel="stylesheet">
+
+</head>
+
+<body>
+<div class="container" style="background-color:red">
+aaaaaaa
+</div>
+	
+</body>
+</html>
